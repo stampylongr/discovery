bd34027905865c7fb67cfe5208743873233e3ab6
lunaisnotaboy <her@mint.lgbt>
2020-10-13 18:26:30 +0000

    update discovery.cr to add stuff

--- src/discovery.cr
+++ src/discovery.cr
@@ -1,6 +1,29 @@
-# TODO: Write documentation for `Discovery`
-module Discovery
-  VERSION = "0.1.0"
+require "kemal"
+require "pg"
+require "yaml"
 
-  # TODO: Put your code here
-end
+CONFIG_STR = ENV.has_key?(ENV_CONFIG_NAME) ? ENV.fetch(ENV_CONFIG_NAME) : File.read("config/config.yml")
+CONFIG     = Config.from_yaml(CONFIG_STR)
+
+PG_URL = URI.new(
+  scheme: "postgres",
+  user: CONFIG.db.user,
+  password: CONFIG.db.password,
+  host: CONFIG.db.host,
+  port: CONFIG.db.port,
+  path: CONFIG.db.dbname,
+)
+
+PG_DB           = DB.open PG_URL
+
+CURRENT_BRANCH  = {{ "#{`git branch | sed -n '/* /s///p'`.strip}" }}
+CURRENT_COMMIT  = {{ "#{`git rev-list HEAD --max-count=1 --abbrev-commit`.strip}" }}
+CURRENT_VERSION = {{ "#{`git describe --tags --abbrev=0`.strip}" }}
+
+ASSET_COMMIT = {{ "#{`git rev-list HEAD --max-count=1 --abbrev-commit -- assets`.strip}" }}
+
+SOFTWARE = {
+  "name"    => "discovery",
+  "version" => "#{CURRENT_VERSION}-#{CURRENT_COMMIT}",
+  "branch"  => "#{CURRENT_BRANCH}",
+}
